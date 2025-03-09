925bc617f153b5d7a3209366a18f121f4bbfc92e
Vi Tran <122925520+vkt5650@users.noreply.github.com>
2025-03-09 09:12:59 +0000

    updated

--- forager/app/photosearch/page.jsx
+++ forager/app/photosearch/page.jsx
@@ -1,22 +1,24 @@
 import TopMenu from "@/components/PhotoSearch/CameraTopMenu";
 import BottomMenu from "@/components/PhotoSearch/CameraBottomMenu";
-import FocusFrame from "@/components/PhotoSearch/FocusFrame";
 import Image from "next/image";
 
 export default function PhotoSearchPage() {
   return (
     <div className="relative w-full h-screen flex flex-col overflow-hidden">
-      <div className="absolute inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 flex items-center justify-center z-0">
         <Image src="/images/camera_mushroom.png" alt="Photosearch background" fill className="w-full h-full object-cover"/>
       </div>
-
+      
       <TopMenu />
-      <FocusFrame />
+      <div className="relative z-10 flex-grow flex items-center justify-center">
+        <img src="/icons/square.svg" alt="Focus frame" />
+      </div>
 
-      <div className="flex-grow"></div>
-      <div className="absolute bottom-0 w-full">
+      <div className="absolute bottom-0 w-full z-10">
         <BottomMenu />
       </div>
     </div>
   );
 }
+
+
--- forager/components/PhotoSearch/FocusFrame.jsx
+++
@@ -1,7 +0,0 @@
-export default function FocusFrame() {
-  return (
-      <div className="fixed inset-0 flex justify-center items-center -top-24">
-          <div className="w-64 h-64 border-2 border-white"></div> 
-      </div>
-  );
-}
